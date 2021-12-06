212a902747d7c3742f5c71818ea718886188f309
WillyS <hsapag@hotmail.com>
2021-12-05 22:00:10 -0300

    [creando navbar]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://guillermohalisapag.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Guillermo Sapag -Desarrollador Web-
-        </a>
-      </header>
-    </div>
-  );
+return(
+<>
+<Navbar/>
+</>
+)
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+const Navbar = () => {
+    return (
+        <div id="elMenu">
+            <h1>Cuadros "Estilo Bruder"</h1>
+            <ul>
+                <li>
+                    HOME
+                </li>
+                <li>
+                    DISEÑOS
+                </li>
+                <li>
+                    TIENDA
+                </li>
+                <li>
+                    CONTACTO
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
