6051b0dcb7a8e804660b62e7005f7d71725e6451
WillyS <hsapag@hotmail.com>
2021-12-24 16:13:49 -0300

    [List, e Item VHS]

--- src/ItemListContainer/List.js
+++ src/ItemListContainer/List.js
@@ -6,9 +6,9 @@ const List = () => {
   const obj = {
     nombre: 'Scream',
     precio: '$ 2500',
+    detalle: 'Duración: 120 minutos, Año: 1997.',
+    stock: '10'
   }
- 
-
 
   return (
     <div>
