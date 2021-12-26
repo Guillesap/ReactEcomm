fb2a1d6b00c00d48758759eb0e8fbf7823e0f47e
WillyS <hsapag@hotmail.com>
2021-12-26 02:22:34 -0300

    [Desafio 7ItemLIst y Lista de Culto]

--- src/components/Peliculas.js
+++ src/components/Peliculas.js
@@ -6,6 +6,7 @@ const Peliculas = () => {
         <img src="https://p4.wallpaperbetter.com/wallpaper/556/569/224/halloween-movie-2018-4k-wallpaper-preview.jpg"/>
        
     )
+    
 }
 
 export default Peliculas
