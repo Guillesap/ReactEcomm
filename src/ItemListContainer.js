e7e99c821370b48727ce0feea2826eb0dfbd3b42
WillyS <hsapag@hotmail.com>
2022-01-04 21:21:14 -0300

    [AgregandoVhs]

--- src/ItemListContainer.js
+++ src/ItemListContainer.js
@@ -8,9 +8,17 @@ const productos = [
     { nombre : "Scream" , foto: "./foto4scream.jpg", precio: 2500, stock : 10, info: "120 minutos. Año 1990" },
     { nombre : "Pesadilla", foto:"./foto3pesadilla.jpg" , precio: 2200, stock : 10, info: "110 minutos. Año 1988" },
     { nombre : "El Cubo",foto:"./foto1cubo.jpg" , precio: 2750, stock : 1, info: "105 minutos. Año 1996" },
-    { nombre : "Se lo que hicieron " , foto:"./foto6seloque.jpg", precio: 2500, stock : 2, info: "125 minutos. Año 1995" },
+    { nombre : "Alien " , foto:"./fotoalien.jpg", precio: 2500, stock : 2, info: "125 minutos. Año 1980" },
     { nombre : "Hellraiser" , foto:"./foto2hell.jpg", precio: 3100, stock : 2, info: "130 minutos. Año 1998" },
-    { nombre : "Viernes 13" ,foto:"./foto5viernes.jpg", precio: 1900, stock : 10, info: "117 minutos. Año 1988" }
+    { nombre : "Viernes 13" ,foto:"./foto5viernes.jpg", precio: 1900, stock : 10, info: "117 minutos. Año 1988" },
+    { nombre : "El Cuervo" ,foto:"./fotocrow.jpg", precio: 1200, stock : 8, info: "147 minutos. Año 1988" },
+    { nombre : "Mysery" ,foto:"./fotomisery.jpg", precio: 2500, stock : 10, info: "107 minutos. Año 1988" },
+    { nombre : "Saw" ,foto:"./foto0jigsaw.jpg", precio: 1700, stock : 10, info: "122 minutos. Año 2000" },
+    { nombre : "It" ,foto:"./fotoit.jpg", precio: 2200, stock : 10, info: "118 minutos. Año 1990" },
+    { nombre : "La Cosa" ,foto:"./fotothing.jpg", precio: 1500, stock : 8, info: "107 minutos. Año 1981" },
+    { nombre : "Halloween" ,foto:"./NEW02.jpg", precio: 3100, stock : 1, info: "130 minutos. Año 2021" },
+    { nombre : "Spiral" ,foto:"./NEW03.jpg", precio: 2900, stock : 3, info: "125 minutos. Año 2021" },
+    { nombre : "Resident Evil" ,foto:"./NEW01.jpg", precio: 1900, stock : 10, info: "137 minutos. Año 2201" }
 
 ]
 
