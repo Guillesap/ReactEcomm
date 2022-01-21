1c53cee7868c01a73e1ee15325101874e5043f78
WillyS <hsapag@hotmail.com>
2022-01-21 17:27:55 -0300

    [Desafio Firebase y agregando categorias]

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,7 +2,7 @@ import { useEffect , useState } from "react"
 import ItemList from "./ItemList"
 import { useParams } from "react-router-dom"
 import { db } from "./firebase"
-import { collection , getDocs , getDoc } from "firebase/firestore"
+import { collection , getDocs , query , where } from "firebase/firestore"
 
 
 
@@ -16,26 +16,32 @@ const ItemListContainer = ({ producto }) => {
     useEffect(() => {
 
 
-        const coleccionProductos = collection(db,"productos")
-        const pedido = getDocs(coleccionProductos)
-
-        pedido
-            .then((resultado)=>{
-                const docs = resultado.docs
-                const docs_formateado = docs.map(doc=>{
-                    const producto = {
-                        id : doc.id,
-                        ...doc.data()
-                    }
-                    return producto
+        if(id){
+            const coleccionProductos = collection(db,"productos")
+            const filtro1 = where("categoria","==",id)
+            const filtro2 = where("precio",">",0)
+            const consulta = query(coleccionProductos,filtro1,filtro2)
+            const pedido = getDocs(consulta)
+            pedido
+                .then((resultado)=>{
+                    setProductos(resultado.docs.map(doc=>({id : doc.id,...doc.data()})))
+                    setLoading(false) 
                 })
-                setProductos(docs_formateado)
-                setLoading(false)
-            })
-            .catch((error)=>{
-                console.log(error)
-            })
-        
+                .catch((error)=>{
+                    console.log(error)
+                })
+        }else {
+            const coleccionProductos = collection(db,"productos")
+            const pedido = getDocs(coleccionProductos)
+            pedido
+                .then((resultado)=>{
+                    setProductos(resultado.docs.map(doc=>({id : doc.id,...doc.data()})))
+                    setLoading(false) 
+                })
+                .catch((error)=>{
+                    console.log(error)
+                })
+        }
 
 
     },[id])
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,7 +19,12 @@ const NavBar = () =>{
                         <li className="nav-item">
                         <Link className="nav-link" to ="/categoria/novedades"><h2>Novedades</h2></Link>
                         </li>
-
+                        <li className="nav-item">
+                        <Link className="nav-link" to ="/categoria/clasicos"><h2>Clásicos</h2></Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link className="nav-link" to ="/categoria/paranormal"><h2>Paranormal</h2></Link>
+                        </li>  
                     </ul>
                     <div id= "elCarrito">
                     <Link to={'/Cart'}><CartWidget /></Link>
